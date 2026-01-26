0accaef579382721afea7184a473645f521fb90d
Văn Trung <162585208+azullshin1109@users.noreply.github.com>
2026-01-26 19:44:38 +0700

    Create Demo2.java

---
+++ Demo/src/Demo2.java
@@ -0,0 +1,20 @@
+public class Demo2 {
+    public static void main(String[] args){
+        String hoten = "Thái Văn Trung";
+        int tuoi = 20;
+
+        //Su dung print
+        System.out.print("Họ Tên: ");
+        System.out.print(hoten);
+        System.out.print(" Năm nay: ");
+        System.out.print(tuoi);
+
+
+        //Su dung println
+        System.out.println( hoten + " năm nay " +tuoi+ " tuoi ");
+
+        //Su dung printf
+        System.out.printf("%s năm nay %d tuổi", hoten, tuoi);
+
+    }
+}
