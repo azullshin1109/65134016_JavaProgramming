14e1d4c840c926ff83606eb169ae88714329f0c3
Văn Trung <162585208+azullshin1109@users.noreply.github.com>
2026-03-02 20:05:09 +0700

    Code

---
+++ Demo/src/Demo6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Demo6 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap so: ");
+        double n = sc.nextDouble();
+
+    }
+}
